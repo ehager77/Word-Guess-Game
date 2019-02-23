b66601486c7603974c48ce2251e5ed80c940adac
ehager77 <eric@isabel.io>
2019-02-23 01:09:07 -0800

    populated wrong and right guesses

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,16 +1,25 @@
 // Pick a random word
 var words = ["Awkward", "Bagpipes", "Crypt", "Dwarves", "Fishhook", "Gazebo", "Ivory", "Jukebox", "Kayak", "Pixel", "Zombie", "Sphinx", "Jinx", "Quip", "Mystify"];
 var wordsIndex = Math.floor(Math.random() * words.length);
-var chosenWord=words[wordsIndex];
+var chosenWord=words[wordsIndex].toLowerCase();
 var rightWord =[];
 var wrongWord = [];
 var underscore = [];
-console.log(chosenWord); 
+var guesses=12;
+var wins = 0;
+console.log(chosenWord);
+
+// DOM manipulation
+
+
+// Main
+
+
 
 // for loop to gen underscore and push to the underscore array
 var generateUnderscore = function() {
     for(var i = 0; i < chosenWord.length;i++){
-        underscore.push("_"); 
+        underscore.push("_");
     }
     return underscore;
 }
@@ -23,16 +32,26 @@ document.addEventListener('keypress', (event) => {
     var letter = String.fromCharCode(event.keyCode);
     // if guess is correct
     if(chosenWord.toLowerCase().indexOf(letter) > -1){
+        // add to right words array
         rightWord.push(letter);
-        console.log(rightWord);
+        // replace underscore with keypress
+        underscore[chosenWord.toLowerCase().indexOf(letter)] = letter;
+        var underScoreDOM = document.getElementById("rightGuess");
+        underScoreDOM.textContent = underscore.join(' ');
+        console.log(underscore);
     }
-
+    // if guess is wrong
     else{
         wrongWord.push(letter);
-        console.log(wrongWord);
+        var wrongGuess = document.getElementById("wrongGuess");
+        wrongGuess.textContent = wrongWord.join(', ');
     }
 
-
+    if(underscore.join('') == chosenWord){
+        var message = document.getElementById("next");
+        message.textContent = "You Win!  Play Again?";
+        wins++;
+    }
 
 });
 
