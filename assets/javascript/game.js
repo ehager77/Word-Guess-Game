0bfb02ebc0fee7008568a050210ed5013f6014f9
ehager77 <eric@isabel.io>
2019-02-23 21:06:08 -0800

    updated logic to prevent multiple bad entries.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,20 +1,19 @@
 // Pick a random word
-var words = ["Crypt", "Dwarves", "Gazebo", "Ivory", "Jukebox", "Pixel", "Zombie", "Sphinx", "Jinx", "Quip"];
+var words = ["Awkward", "Jinx", "Quip"];
 var wordsIndex = Math.floor(Math.random() * words.length);
 var chosenWord=words[wordsIndex].toLowerCase();
-var rightWord =[];
+//var rightWord =[];
 var wrongWord = [];
 var underscore = [];
+var correctGuesses = 0;
 var guesses=12;
+console.log(guesses);
 console.log(chosenWord);
+var wins=0;
 
-// DOM manipulation
+var guesses = document.getElementById("remaining-guesses").innerHTML;
 
-
-// Main
-
-var guesses = document.getElementById("remainng-guesses");
-
+function main(){
 
 
 // for loop to gen underscore and push to the underscore array
@@ -22,6 +21,7 @@ var generateUnderscore = function() {
     for(var i = 0; i < chosenWord.length;i++){
         underscore.push("_");
     }
+
     return underscore;
 }
 
@@ -32,28 +32,71 @@ console.log(generateUnderscore());
 document.addEventListener('keypress', (event) => {
     var letter = String.fromCharCode(event.keyCode);
     // if guess is correct
+    for(var i = 0; i <chosenWord.length; i++){
+        if(chosenWord[i] === letter){
+            underscore[i] = letter;
+            // rightWord.push(letter);
+            console.log(underscore);
+        }
+        // If guess is wrong
+        // else {
+        //     var wrongGuess = document.getElementById("wrongGuess");
+        //     console.log(wrongGuess);
+        //     wrongGuess.textContent = wrongWord.join(', ');
+        //     guesses--;
+        //     console.log(guesses);
+            
+        // }
+
+    }
+    
+
+
     if(chosenWord.toLowerCase().indexOf(letter) > -1){
         // add to right words array
-        rightWord.push(letter);
         // replace underscore with keypress
         underscore[chosenWord.toLowerCase().indexOf(letter)] = letter;
         var underScoreDOM = document.getElementById("rightGuess");
         underScoreDOM.textContent = underscore.join(' ');
-        console.log(underscore);
     }
     // if guess is wrong
     else{
-        wrongWord.push(letter);
-        var wrongGuess = document.getElementById("wrongGuess");
-        wrongGuess.textContent = wrongWord.join(', ');
+        guesses--;
+        //wrongWord.push(letter);
+        console.log(guesses)
+        console.log(wrongWord);
+        if(wrongWord.indexOf(letter) === -1){
+            wrongWord.push(letter);
+            var wrongGuess = document.getElementById("wrongGuess");
+            wrongGuess.textContent = wrongWord.join(', ');
+
+        }else{
+            alert("You suck.");
+        }
+        // for(var i=0; i< underscore.length; i++){
+        //     if(wrongWord[i] === letter){
+        //         //console.log(wrongWord[i]);
+        //         alert("You suck.")
+        //     }
+        //     else if(wrongWord.indexOf(letter)===-1){
+        //         wrongWord.push(letter);
+        //         console.log(wrongWord);
+        //         var wrongGuess = document.getElementById("wrongGuess");
+        //         wrongGuess.textContent = wrongWord.join(', ');
+        //     }
+            
+        // }
+        
     }
 
     if(underscore.join('') == chosenWord){
         var message = document.getElementById("next");
-        message.textContent = "You Win!  Play Again?";
-        var wins=0;
+        message.textContent = "You Win! Press Refresh to Play Again.";
         var winsCount = document.getElementById("wins-count").innerHTML=wins+1;
+    }  
+
     }
 
-});
+ );
 
+}
